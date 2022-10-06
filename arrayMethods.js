61af19e022597a2e2d38682645ad982c52be15a7
Cameron Miner <cameronsminer@gmail.com>
2022-10-05 20:16:05 -0600

    Saving progress

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,7 +18,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-// const evenNumbers = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(element => element % 2 === 0)
+
+console.log(evenNumbers)
 
 
 
@@ -39,9 +41,12 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-// const postTaxPrices = prices.map(/* Provide Your Callback Here );
+const postTaxPrices = prices.map(function(element) {
+  return element * 1.07
+});
 
 
+console.log(postTaxPrices)
 
 ////////// PROBLEM 3 //////////
 
@@ -57,7 +62,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-// const totalPopulation = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation = populations.reduce(/* Provide Your Callback Here */)
 
 
 
@@ -82,13 +87,29 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-function callback(monster) {
-  return monster.CP > 200
-}
 
-const myStrongest = monstersInYourPocket.filter(callback)
 
-console.log(myStrongest)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function callback(monster) {
+//   return monster.CP > 200
+// }
+
+// const myStrongest = monstersInYourPocket.filter(callback)
+
+// console.log(myStrongest)
 
 ////////// PROBLEM 5 //////////
 
